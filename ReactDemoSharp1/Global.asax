9de4346b8cde05fc7b999f152f982af55cc8c785
zakizadeh <hussein.zakizadeh@gmail.com>
2018-09-08 01:09:16 -0700

    Add project files.

---
+++ ReactDemoSharp1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ReactDemoSharp1.MvcApplication" Language="C#" %>
---
+++ ReactDemoSharp1/Scripts/Tutorial.jsx
@@ -0,0 +1,21 @@
+﻿var CommentBox = React.createClass({  
+    render: function() {  
+        return (  
+          <div className="commentBox">  
+            Hello, world! This is barnamenevisan  
+          </div>  
+      );  
+    }  
+});  
+ReactDOM.render(  
+  <CommentBox />,  
+  document.getElementById('content')  
+);  
+
+
+
+
+
+
+
+
